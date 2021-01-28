025b98ae042c134fbf609600502ecc388e2952c6
LW-MrWU <651757920@qq.com>
2021-01-28 13:24:11 +0800

    bstats

--- src/main/java/vip/gameclub/lwlib/llb/LwLibMainPlugin.java
+++ src/main/java/vip/gameclub/lwlib/llb/LwLibMainPlugin.java
@@ -2,6 +2,7 @@ package vip.gameclub.lwlib.llb;
 
 import vip.gameclub.lwlib.model.enumModel.BaseSysMsgEnum;
 import vip.gameclub.lwlib.service.plugin.BasePlugin;
+import vip.gameclub.lwlib.service.utils.Metrics;
 
 /**
  * lwlib服务主类
@@ -28,8 +29,13 @@ public class LwLibMainPlugin extends BasePlugin {
 
         //初始化配置文件
         initConfig();
+
+        //初始化统计
+        initBstats();
+
         //成功加载提示
         getBaseLogService().infoByLanguage(BaseSysMsgEnum.SUCCESS_LOAD.name(), BaseSysMsgEnum.SUCCESS_LOAD.getValue());
+
         return true;
     }
 
@@ -47,5 +53,20 @@ public class LwLibMainPlugin extends BasePlugin {
      */
     private void initConfig(){
         //LwLibDefaultConfig defaultConfig = new LwLibDefaultConfig();
+    }
+
+    /**
+     * 初始化统计
+     * @param
+     * @return void
+     * @author LW-MrWU
+     * @date 2021/1/28 13:21
+     */
+    private void initBstats(){
+        int pluginId = 10141;
+        Metrics metrics = new Metrics(this, pluginId);
+
+        // Optional: Add custom charts
+        //metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "My value"));
     }
 }
