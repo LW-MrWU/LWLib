b3b1ed3113ea6a5eeac25f4052bb1483a64e4e32
LW-MrWU <651757920@qq.com>
2021-01-28 12:22:52 +0800

    javadoc

--- src/main/java/vip/gameclub/lwlib/event/BaseEvent.java
+++ src/main/java/vip/gameclub/lwlib/event/BaseEvent.java
@@ -4,9 +4,9 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 
 /**
+ * 事件父类
  * @author LW-MrWU
  * @date 创建时间 2021/1/18 18:50
- * 事件父类
  */
 public abstract class BaseEvent extends Event implements Cancellable {
 }
--- src/main/java/vip/gameclub/lwlib/event/package-info.java
+++ src/main/java/vip/gameclub/lwlib/event/package-info.java
@@ -1,6 +1,6 @@
 /**
+ * 事件包
  * @author LW-MrWU
  * @date 创建时间 2021/1/28 10:59
- * 事件包
  */
 package vip.gameclub.lwlib.event;
